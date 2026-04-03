3448a10c55abb27cce73c10ec76b045952a15295
avery-browning <avery.o.browning@gmail.com>
2026-04-02 21:56:00 -0400

    +

--- 2026-03-24_class_script_missed.R
+++ 2026-03-24_class_script_missed.R
@@ -1,1 +1,13 @@
 # 2026-03-24 class script - missed
+
+load("data/RAMLDB v4.66/R Data/DBdata[asmt][v4.66].RData")
+
+head(tsmetrics)
+head(timeseries)
+
+timeseries_tsmetrics = left_join(timeseries, tsmetrics, by = c("tsid" = "tsunique"))
+dim(timeseries)
+dim(timeseries_tsmetrics)
+head(timeseries_tsmetrics)
+
+# come back to zoom recording at 10 minute mark 
